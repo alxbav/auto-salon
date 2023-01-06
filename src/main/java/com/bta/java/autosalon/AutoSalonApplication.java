528e4764dab0807cb83fd28f2b7302754a453c3f
Oleg Pasichnyk <oleg.pasichnyk@kuehne-nagel.com>
2023-01-06 16:10:07 +0200

    - Fixed dependencies; - added docker compose file for run DB locally from Docker image.

--- src/main/java/com/bta/java/autosalon/AutoSalonApplication.java
+++ src/main/java/com/bta/java/autosalon/AutoSalonApplication.java
@@ -5,12 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AutoSalonApplication {
-
-
     public static void main(String[] args) {
         SpringApplication.run(AutoSalonApplication.class, args);
     }
-
 
 }
 
