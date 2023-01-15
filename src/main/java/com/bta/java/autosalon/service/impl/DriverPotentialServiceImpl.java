ebcbd1780094b690da3e80c90382bddd06db8756
alxbav <avbezrukov@gmail.com>
2023-01-15 11:45:31 +0200

    1

--- src/main/java/com/bta/java/autosalon/service/impl/DriverPotentialServiceImpl.java
+++ src/main/java/com/bta/java/autosalon/service/impl/DriverPotentialServiceImpl.java
@@ -15,14 +15,15 @@ public class DriverPotentialServiceImpl implements DriverPotentialService {
 
     @Override
     public void add(DriverPotentialRequestDto dto) {
-  //      validate(dto);
-  //      validateUniquenessByPhone(dto);
+        validate(dto);
+        validateUniquenessByPhone(dto);
+        validateSizeComment(dto);
         final var entity = mapToEntity(dto);
         driverPotentialRepository.save(entity);
     }
 
 
-    private DriverPotential mapToEntity(DriverPotentialRequestDto dto){
+    private DriverPotential mapToEntity(DriverPotentialRequestDto dto) {
         return DriverPotential.builder()
                 .firstName(dto.getFirstName())
                 .lastName(dto.getLastName())
@@ -32,20 +33,31 @@ public class DriverPotentialServiceImpl implements DriverPotentialService {
                 .build();
     }
 
-    private void validateUniquenessByPhone(DriverPotentialRequestDto dto){
+    private void validateSizeComment(DriverPotentialRequestDto dto) {
+        String comment = dto.getComment();
+        if (comment.length() >= 100) {
+            throw new RuntimeException(" Comment is very long! Maximum length -  100 symbols! ");
+        }
+
+    }
+
+
+    private void validateUniquenessByPhone(DriverPotentialRequestDto dto) {
         var drivers = driverPotentialRepository.findByPhone(dto.getPhone());
-        if(!drivers.isEmpty()) {
+        if (!drivers.isEmpty()) {
             throw new RuntimeException("Driver with MP already exists..");
         }
     }
 
-    private void validate(DriverPotentialRequestDto dto){
+    private void validate(DriverPotentialRequestDto dto) {
         String firstName = dto.getFirstName();
-        if(firstName==null || firstName.isEmpty()) {
+        if (firstName == null || firstName.isEmpty()) {
             throw new RuntimeException("FirstName must be filled!!");
-            //TODO ..
         }
-        //check size
+        if (firstName.length() >= 15) {
+            throw new RuntimeException("FirstName is very long. Maximum length - 15 symbols!");
+        }
+
 
     }
 }
